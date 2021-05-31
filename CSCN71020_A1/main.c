39138b49419a4096b11379deba64c999173e9ef7
Elliott Coleshill <elliott.coleshill@gmail.com>
2021-05-31 14:50:31 -0400

    Commit #2 - What was added?

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 
 void main() {
@@ -14,12 +15,14 @@ void main() {
 	int inputNum;
 
 	printf("Enter operation number: ");
-	scanf_s("%1o", &inputNum);
+	scanf("%1o", &inputNum);
 
 	switch (inputNum)
 	{
 	case 1:
 		add();
+	case 2:
+		subtract();
 	}
 
 }
@@ -39,9 +42,18 @@ void printOptions() {
 void add() {
 	double num1, num2, result;
 	printf("Enter the first value:");
-	scanf_s("%lf", &num1);
+	scanf("%lf", &num1);
 	printf("Enter the second value:");
-	scanf_s("%lf", &num2);
+	scanf("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract() {
+	int num1, num2;
+
+	num1 = 0;
+	num2 = 42;
+
+	int result = num2 / num1;
 }
